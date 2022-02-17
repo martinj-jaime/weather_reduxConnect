e71150a8edc3f3a0d80d9893d87be7364019a3e2
martin jaime <martinjosejaime@gmail.com>
2022-02-17 17:41:33 -0300

    Refactoring ForecastExtended component

--- src/containers/ForecastExtendedContainer.jsx
+++ src/containers/ForecastExtendedContainer.jsx
@@ -7,19 +7,26 @@ import { connect } from 'react-redux'
 class ForecastExtendedContainer extends Component {
 
   render() {
+    const { city, forecastData } = this.props
     return (
-        this.props.city &&
-        <ForecastExtended city={this.props.city} />
+        city &&
+        <ForecastExtended 
+        city={city} 
+        forecastData={forecastData} 
+        />
     )
   }
 }
 
 ForecastExtendedContainer.propTypes = {
-    city: PropTypes.string.isRequired
+    city: PropTypes.string.isRequired,
+    forecastData: PropTypes.array.isRequired
 }
 
 // mapStateToProps = recibe el estado
 // const mapStateToProps = (state) => ({ city: state.city })
-const mapStateToProps = ({ city }) => ({ city })
+const mapStateToProps = ({ city, cities }) => (
+  { city, forecastData: cities[city].forecastData }
+)
 
 export default connect(mapStateToProps)(ForecastExtendedContainer)
