aeb396d5acfef63b512be0d3a0f50e510300e455
martin jaime <martinjosejaime@gmail.com>
2022-02-17 15:55:24 -0300

    Adding redux-thunk middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,15 @@
-import { createStore } from 'redux'
-
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 import { city } from '../reducers/city'
 
 const initialState = {
   city: 'Buenos Aires,ar'
 }
 
+// para ver graficamente la app
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 export const store = createStore(city, initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(applyMiddleware(thunk))
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
